360724fd1a53f220679b32d47023c004c263422d
Shubhanshu Mishra <shubhanshumishra@gmail.com>
2020-07-17 01:13:30 -0500

    Update socialmediaie_setup.sh

--- docs/IC2S2_2020/socialmediaie_setup.sh
+++ docs/IC2S2_2020/socialmediaie_setup.sh
@@ -1,4 +1,12 @@
-# Install miniconda before running this. 
+# If you are using Mac or Linux you can simply run this script on the whole. 
+# If you are on Windows you need to have git installed from https://git-scm.com/downloads then you can launch git Bash
+# Windows users: run the commands in their respective applications, i.e. git bash and Anaconda Prompt
+
+set -e
+# Ensure that you have installed Miniconda or Anaconda before running the following commands.
+# You can check it by running the following commands
+conda --version
+
 # Create a folder called IC2S2 and run all the commands withing that folder
 # Run this in git bash or download the repository
 git clone https://github.com/socialmediaie/SocialMediaIE.git
